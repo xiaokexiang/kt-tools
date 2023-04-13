9b5407d174de246ac35a1ef41a270b84e89aa868
xiaokexiang <xxiaokexiang@gmail.com>
2023-04-13 22:48:48 +0800

    添加图形化界面 20%

--- qt.py
+++ qt.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+from functools import reduce
 
 from PyQt5.QtCore import QRegExp
 from PyQt5.QtGui import QRegExpValidator
@@ -8,6 +9,8 @@ from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QLabel, QLineEdit, Q
     QGridLayout, QVBoxLayout, QHBoxLayout, QMessageBox, QComboBox
 
 json_path = './servers.json'
+
+
 class Main(QWidget):
     def __init__(self):
         super(Main, self).__init__()
@@ -33,6 +36,8 @@ class Main(QWidget):
         self.h_layout = QHBoxLayout()
         self.v_layout = QVBoxLayout()
 
+        self.servers = []
+        self.servers_dict = {}
         self.line_edit_init()
         self.button_init()
         self.layout_init()
@@ -55,6 +60,10 @@ class Main(QWidget):
 
         self.setLayout(self.v_layout)
 
+    """
+    下拉框初始化
+    """
+
     def combo_box_init(self):
         self.comboBox.setEditable(True)
         self.comboBox.lineEdit().setPlaceholderText('请输入ip地址')
@@ -62,28 +71,65 @@ class Main(QWidget):
                                                             r'3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')))
         if not os.path.exists(json_path) or 0 == os.path.getsize(json_path):
             with open(json_path, 'w') as f:
-                f.write('{"servers": []}')
+                pass
         # 此处读取数据库中的数据
         with open(json_path, encoding='utf-8') as f:
-            data = [item['ip'] for item in dict(json.loads(f.read())).get('servers')]
+            self.servers = reduce(
+                lambda y, x: y if (x['ip'] in [i['ip'] for i in y]) else (lambda z, u: (z.append(u), z))(y, x)[1],
+                json.loads(f.read()), [])
+            data = [item['ip'] for item in self.servers]
+            servers_dict = {data[i]: self.servers[i] for i in range(len(self.servers))}
+            self.servers_dict = servers_dict
+        # 根据ip回显其他数据
+        if data.__len__() != 0:
+            self.username_line.setText(servers_dict[data[0]]['username'])
+            self.password_line.setText(servers_dict[data[0]]['password'])
+            self.image_line.setText(servers_dict[data[0]]['image'])
         self.comboBox.addItems(data)
-        self.comboBox.currentIndexChanged.connect(lambda: self.on_combobox_func(self.comboBox))
+        self.comboBox.currentIndexChanged.connect(lambda: self.on_combobox_func(self.comboBox, servers_dict))
+        self.comboBox.currentTextChanged.connect(lambda: self.on_combobox_func(self.comboBox, servers_dict))
 
-    def on_combobox_func(self, combo_box):
-        QMessageBox.information(self, 'Information', combo_box.currentText())
+    """
+    下拉框校验与回显
+    """
+
+    def on_combobox_func(self, combo_box, servers_dict):
+        if len(combo_box.currentText()) != 0 and combo_box.currentText() != '' and combo_box.currentText() in servers_dict:
+            self.username_line.setText(servers_dict[combo_box.currentText()]['username'])
+            self.password_line.setText(servers_dict[combo_box.currentText()]['password'])
+            self.image_line.setText(servers_dict[combo_box.currentText()]['image'])
+        else:
+            self.username_line.clear()
+            self.password_line.clear()
+            self.image_line.clear()
+
+    """
+    编辑框初始化
+    """
 
     def line_edit_init(self):
-        # self.comboBox.setPlaceholderText('请输入ip地址')
         self.username_line.setPlaceholderText('请输入账户(不填默认root)')
+        self.username_line.setMaxLength(10)
         self.password_line.setPlaceholderText('请输入密码')
+        self.username_line.setMaxLength(15)
         self.image_line.setPlaceholderText('请输入镜像(默认abcsys.cn:5000/public/kt-connect-shadow:stable)')
-        self.comboBox.currentIndexChanged.connect(self.check_input_func)
+        self.comboBox.currentTextChanged.connect(self.check_input_func)
         self.username_line.textChanged.connect(self.check_input_func)
+        self.password_line.textChanged.connect(self.check_input_func)
+        self.image_line.textChanged.connect(self.check_input_func)
+
+    """
+    按钮初始化
+    """
 
     def button_init(self):
         self.start_button.setEnabled(False)
         self.start_button.clicked.connect(self.check_sign_in_func)
         self.add_button.clicked.connect(self.show_add_page)
+
+    """
+    编辑框校验
+    """
 
     def check_input_func(self):
         if self.comboBox.currentText() and self.username_line.text() and self.password_line.text() and self.image_line.text():
@@ -91,14 +137,34 @@ class Main(QWidget):
         else:
             self.start_button.setEnabled(False)
 
+    """
+    启动逻辑
+    """
+
     def check_sign_in_func(self):
-        if self.comboBox.text() != 'a' or self.username_line.text() != 'a':
-            QMessageBox.critical(self, 'Wrong', 'Two Passwords Typed Are Not Same!')
-        else:
+        self.start_button.setEnabled(False)
+        # 校验数据是否存在
+        if not self.comboBox.currentText() in self.servers_dict:
+            new_item = {
+                "ip": self.comboBox.currentText(),
+                "username": self.username_line.text(),
+                "password": self.password_line.text(),
+                "image": self.image_line.text()
+            }
+            self.servers_dict[self.comboBox.currentText()] = new_item
+            self.servers.append(new_item)
+            with open(json_path, 'w') as file:
+                json.dump(self.servers, file)
+
             QMessageBox.information(self, 'Information', 'Register Successfully')
-            self.close()
-        self.comboBox.clear()
-        self.username_line.clear()
+            # self.close()
+        # self.start_button.setEnabled(True)
+        # self.comboBox.clear()
+        # self.username_line.clear()
+        # self.comboBox.lineEdit().setEnabled(False)
+        # self.username_line.setEnabled(False)
+        # self.password_line.setEnabled(False)
+        # self.image_line.setEnabled(False)
 
     def show_add_page(self):
         self.add_page.exec_()
